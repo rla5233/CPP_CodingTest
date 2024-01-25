4585570be8959709967478a8625061569e531e81
rla5233 <rla5233@naver.com>
2024-01-25 10:32:36 +0900

    BJO 14490

--- CPP_CodingTest/CPP_CodingTest/CPP_CodingTest.cpp
+++ CPP_CodingTest/CPP_CodingTest/CPP_CodingTest.cpp
@@ -1,26 +1,57 @@
 ﻿#include <iostream>
-#include <vector>
+#include <string>
 
-// 11659
+int GCD(int _A, int _B);
+
+// 14490
 int main()
 {
-    std::ios_base::sync_with_stdio(false); std::cin.tie(NULL); std::cout.tie(NULL);
-    int N = 0, M = 0;
-    std::cin >> N >> M;
+	std::string Num = "";
+	std::cin >> Num;
 
-    std::vector<int> Sum(N + 1, 0);
-    for (int i = 0; i < N; i++)
-    {
-        int Num = 0;
-        std::cin >> Num;
-        Sum[i + 1] = Sum[i] + Num;
-    }
+	bool IsN = true;
+	int N = 0, M = 0;
+	std::string N_Str = "", M_Str = "";
+	for (int i = 0; i < Num.size(); i++)
+	{
+		if (Num[i] == ':')
+		{
+			IsN = false;
+			continue;
+		}
 
-    for (int i = 0; i < M; i++)
-    {
-        int Left = 0, Right = 0;
-        std::cin >> Left >> Right;
+		if (IsN)
+		{
+			N_Str += Num[i];
+		}
+		else
+		{
+			M_Str += Num[i];
+		}
+	}
 
-        std::cout << Sum[Right] - Sum[Left - 1] << "\n";
-    }
+	N = stoi(N_Str);
+	M = stoi(M_Str);
+
+	int gcd = 0;
+	gcd = GCD(N, M);
+	
+	N /= gcd;
+	M /= gcd;
+	
+	std::string Result = "";
+	Result += std::to_string(N) + ":" + std::to_string(M);
+	std::cout << Result << std::endl;
+}
+
+int GCD(int _A, int _B)
+{
+	int r;
+	while (_B != 0)
+	{
+		r = _A % _B;
+		_A = _B;
+		_B = r;
+	}
+	return _A;
 }
