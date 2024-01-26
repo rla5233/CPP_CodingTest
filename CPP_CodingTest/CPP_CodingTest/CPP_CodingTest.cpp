562161685c7c325fd78265521af7b372693e34f5
rla5233 <rla5233@naver.com>
2024-01-26 12:45:46 +0900

    BJO 2959

--- CPP_CodingTest/CPP_CodingTest/CPP_CodingTest.cpp
+++ CPP_CodingTest/CPP_CodingTest/CPP_CodingTest.cpp
@@ -1,57 +1,18 @@
 ﻿#include <iostream>
-#include <string>
+#include <vector>
+#include <algorithm>
 
-int GCD(int _A, int _B);
-
-// 14490
+// 2959
 int main()
 {
-	std::string Num = "";
-	std::cin >> Num;
-
-	bool IsN = true;
-	int N = 0, M = 0;
-	std::string N_Str = "", M_Str = "";
-	for (int i = 0; i < Num.size(); i++)
+	//std::ios_base::sync_with_stdio(false); std::cin.tie(NULL); std::cout.tie(NULL);
+	std::vector<int> Num(4, 0);
+	for (int i = 0; i < 4; i++)
 	{
-		if (Num[i] == ':')
-		{
-			IsN = false;
-			continue;
-		}
-
-		if (IsN)
-		{
-			N_Str += Num[i];
-		}
-		else
-		{
-			M_Str += Num[i];
-		}
+		std::cin >> Num[i];
 	}
 
-	N = stoi(N_Str);
-	M = stoi(M_Str);
+	sort(Num.begin(), Num.end());
 
-	int gcd = 0;
-	gcd = GCD(N, M);
-	
-	N /= gcd;
-	M /= gcd;
-	
-	std::string Result = "";
-	Result += std::to_string(N) + ":" + std::to_string(M);
-	std::cout << Result << std::endl;
-}
-
-int GCD(int _A, int _B)
-{
-	int r;
-	while (_B != 0)
-	{
-		r = _A % _B;
-		_A = _B;
-		_B = r;
-	}
-	return _A;
+	std::cout << Num[0] * Num[2];
 }
