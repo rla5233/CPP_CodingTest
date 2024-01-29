2f9096819dee1f471c2bdd12de49ae40848f3f9d
rla5233 <rla5233@naver.com>
2024-01-29 11:23:04 +0900

    BJO 9322

--- CPP_CodingTest/CPP_CodingTest/CPP_CodingTest.cpp
+++ CPP_CodingTest/CPP_CodingTest/CPP_CodingTest.cpp
@@ -1,19 +1,45 @@
 ﻿#include <iostream>
-#include <algorithm>
+#include <vector>
+#include <map>
 
-// 2992
+// 9322
 int main()
 {
     //std::ios_base::sync_with_stdio(false); std::cin.tie(NULL); std::cout.tie(NULL);
-    std::string Num = "";
-    std::cin >> Num;
+    int TestCase = 0;
+    std::cin >> TestCase;
 
-    if (next_permutation(Num.begin(), Num.end()))
+    for (int i = 0; i < TestCase; i++)
     {
-        std::cout << Num;
-    }
-    else
-    {
-        std::cout << "0";
+        int WordCount = 0;
+        std::cin >> WordCount;
+
+        std::string Word = "";
+        std::map<std::string, int> FirstKey;
+        for (int idx = 0; idx < WordCount; idx++)
+        {
+            std::cin >> Word;
+            FirstKey[Word] = idx;
+        }
+
+        std::vector<int> Order(WordCount);
+        for (int idx = 0; idx < WordCount; idx++)
+        {
+            std::cin >> Word;
+            Order[idx] = FirstKey[Word];
+        }
+
+        std::vector<std::string> OrgWordVec(WordCount);
+        for (int idx = 0; idx < WordCount; idx++)
+        {
+            std::cin >> Word;
+            OrgWordVec[Order[idx]] = Word;
+        }
+
+        for (std::string& Word : OrgWordVec)
+        {
+            std::cout << Word << " ";
+        }
+        std::cout << "\n";
     }
 }
