56b6108e2eba970db5dd5d556dc07ba86cabc904
김수원 <rla5233@naver.com>
2024-08-20 17:50:55 +0900

    김수원 : 프로그래머스 181897 업데이트

--- CPP_CodingTest/Core/Programmers.cpp
+++ CPP_CodingTest/Core/Programmers.cpp
@@ -50,3 +50,40 @@ int Programmers::Problem_250137(std::vector<int> Bandage, int Health, std::vecto
     return CurHp;
 }
 
+std::vector<int> Programmers::Problem_181897(int N, std::vector<int> Slicer, std::vector<int> Num_list)
+{
+    std::vector<int> Result;
+
+    int Start = 0, End = 0, InterVal = 0;
+    switch (N)
+    {
+    case 1:
+        Start = 0;
+        End = Slicer[1];
+        InterVal = 1;
+        break;
+    case 2:
+        Start = Slicer[0];
+        End = static_cast<int>(Num_list.size() - 1);
+        InterVal = 1;
+        break;
+    case 3:
+        Start = Slicer[0];
+        End = Slicer[1];
+        InterVal = 1;
+        break;
+    case 4:
+        Start = Slicer[0];
+        End = Slicer[1];
+        InterVal = Slicer[2];
+        break;
+    }
+
+    for (int i = Start; i <= End; i += InterVal)
+    {
+        Result.push_back(Num_list[i]);
+    }
+
+    return Result;
+}
+
--- CPP_CodingTest/Core/Programmers.h
+++ CPP_CodingTest/Core/Programmers.h
@@ -16,7 +16,7 @@ public:
 
 public:
 	static int Problem_250137(std::vector<int> Bandage, int Health, std::vector<std::vector<int>> Attacks);
-
+	static std::vector<int> Problem_181897(int N, std::vector<int> Slicer, std::vector<int> Num_list);
 
 };
 
