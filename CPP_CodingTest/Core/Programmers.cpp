43fa6537067558812f5ec9e3586c3c22122763db
김수원 <rla5233@naver.com>
2024-08-29 16:31:33 +0900

    김수원 : 프로그래머스 181832

--- CPP_CodingTest/Core/Programmers.cpp
+++ CPP_CodingTest/Core/Programmers.cpp
@@ -51,6 +51,33 @@ int Programmers::Problem_120956(std::vector<std::string> Babbling)
 
     return Answer;
 }
+std::vector<std::vector<int>> Programmers::Problem_181832(int N)
+{
+    std::vector<std::vector<int>> Answer(N, std::vector<int>(N, -1));
+
+    std::vector<int> dx = { 1, 0, -1, 0 };
+    std::vector<int> dy = { 0, 1, 0, -1 };
+
+    int DirIndex = 0, NCount = 1, y = 0, x = 0;
+    while (N * N >= NCount)
+    {
+        Answer[y][x] = NCount;
+        ++NCount;
+
+        int NextX = x + dx[DirIndex];
+        int NextY = y + dy[DirIndex];
+
+        if (NextX < 0 || NextX >= Answer[y].size() || NextY < 0 || NextY >= Answer.size() || -1 != Answer[NextY][NextX])
+        {
+            DirIndex = ++DirIndex % 4;
+        }
+
+        x += dx[DirIndex];
+        y += dy[DirIndex];
+    }
+
+    return Answer;
+}
 std::vector<int> Programmers::Problem_181897(int N, std::vector<int> Slicer, std::vector<int> Num_list)
 {
     std::vector<int> Result;
